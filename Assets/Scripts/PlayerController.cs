cc739076e67a576fe0c6cc75b4b6ae013d0982d4
Miguel <loranvallsmiguel@gmail.com>
2026-07-21 23:10:12 +0200

    feat: Player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,21 +5,27 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] private float movementSpeed;
     [SerializeField] private float jumpForce;
-    private RigidBody rigidbody;
+    [SerializeField] private float gravity = 9.81f;
+    private CharacterController controller;
+    private Vector3 velocity;
 
-    void Start() => rigidbody = GetComponent<RigidBody>();
+    void Start() => controller = GetComponent<CharacterController>();
 
     void Update()
     {
         // Movement
-        float xAxis = Input.GetAxis("Horizontal") * Time.deltaTime;
-        float zAxis = Input.GetAxis("Vertical") * Time.deltaTime;
+        float xAxis = Input.GetAxisRaw("Horizontal");
+        float zAxis = Input.GetAxisRaw("Vertical");
 
-        transform.Translate(new Vector3(xAxis, 0, zAxis), TransformSpace.Local);
+        Vector3 movement = transform.right * xAxis + transform.forward * zAxis;
+        controller.Move(movement.normalized * movementSpeed * Time.deltaTime);
+
+        // Gravity
+        velocity += Vector3.down * gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
 
         // Jumping
-        if (Input.GetKeyDown(KeyCode.Space))
-            rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        // if (Input.GetKeyDown(KeyCode.Space)) rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
     }
 
 }
